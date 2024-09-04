df0e21c4abe7f76ad20f2875b4d9419f8ebca885
chirag tekwani <ctekwani051@gmail.com>
2024-09-04 11:48:53 +0530

    Create method_inside_class.cpp

---
+++ method_inside_class.cpp
@@ -0,0 +1,25 @@
+//Name:Chirag Tekwani
+//PRN:23070123042
+//Aim:Implemention of Classes and Objects to define method inside class
+
+#include<iostream>
+using namespace std;
+
+class Student               
+{
+    public:
+    void myMethod()
+    {
+        cout<<"I am studying in 2nd year";
+    }
+};
+
+int main()
+{
+    Student s1;
+    s1.myMethod();
+}
+
+
+/*Output:
+I am studying in 2nd year*/
